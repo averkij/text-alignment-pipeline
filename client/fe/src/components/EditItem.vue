aeed82aa52433d6b66a79bc6572a0b8d3e46d9fc
Sergei Averkiev <averoo@gmail.com>
2020-07-22 16:59:12 +0300

    Update layout.

--- client/fe/src/components/EditItem.vue
+++ client/fe/src/components/EditItem.vue
@@ -6,20 +6,23 @@
                     <div class="d-table-cell grey lighten-4 pa-2 text-center" style="min-width:45px">
                         {{ item.line_ids[0] + 1 }}
                     </div>
+                    <v-divider class="d-table-cell" vertical></v-divider>
                     <div class="d-table-cell pa-2">{{ item.text }}</div>
                 </div>
             </v-col>
             <v-col class="text-left" cols="6">
                 <div class="d-table fill-height">
-                    <div class="d-table-cell grey lighten-4 text-center" style="min-width:45px">
-                        <div class="fill-height lighten-4 d-flex flex-column justify-space-between"
-                            :class="{green: item.selected.sim>0.5}">
+                    <v-divider class="d-table-cell" vertical></v-divider>
+                    <div class="d-table-cell grey lighten-5 text-center" style="min-width:45px">
+                        <div class="fill-height lighten-5 d-flex flex-column justify-space-between"
+                            :class="{green: item.selected.sim>0.5, yellow: item.selected.sim<=0.5 & item.selected.sim>0.3}">
                             <div class="pa-2">
                                 {{ selectedLineId }}
                             </div>
                             <div class="text-caption pa-1">{{ item.selected.sim | numeral('0.00') }}</div>
                         </div>
                     </div>
+                    <v-divider class="d-table-cell" vertical></v-divider>
                     <div class="d-table-cell pa-2">
                         {{ item.selected.text }}
                         <!-- <span class="yellow lighten-4">{{ item.trans }}</span> -->

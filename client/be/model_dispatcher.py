55e80a48f46c8eb072d831752e3fb84ec721453f
Sergei Averkiev <averoo@gmail.com>
2020-08-05 13:50:41 +0300

    Update dispathcher.

--- client/be/model_dispatcher.py
+++ client/be/model_dispatcher.py
@@ -1,9 +1,18 @@
+import os
 import pickle
+
 from sentence_transformers import SentenceTransformer
 
 # model = SentenceTransformer('distiluse-base-multilingual-cased')
 # pickle.dump(model, open("sentence_transformers.bin", 'wb'))
 
+SENTENCE_TRANSFORMERS_MODEL_PATH = 'models/sentence_transformers.bin'
+
+if os.path.isfile(SENTENCE_TRANSFORMERS_MODEL_PATH):
+    sentence_transformers_model = pickle.load(open(SENTENCE_TRANSFORMERS_MODEL_PATH, 'rb'))
+else:
+    sentence_transformers_model = SentenceTransformer('distiluse-base-multilingual-cased')
+
 models = {
-    "sentence_transformer_multilang": pickle.load(open('models/sentence_transformers.bin', 'rb'))
+    "sentence_transformer_multilang": sentence_transformers_model
 }

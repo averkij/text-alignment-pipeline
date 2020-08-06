f3d8fa800f1c0df19b07d6272d4cf4df722ce38b
Sergei Averkiev <averoo@gmail.com>
2020-08-06 15:41:51 +0300

    Rearranging.

---
+++ client/fe/app/src/common/config.js
@@ -0,0 +1,3 @@
+// export const API_URL = "http://127.0.0.1:80";
+export const API_URL = "https://align-me-be.herokuapp.com";
+export default API_URL;
--- client/fe/src/common/config.js
+++
@@ -1,2 +0,0 @@
-export const API_URL = "http://127.0.0.1:80";
-export default API_URL;

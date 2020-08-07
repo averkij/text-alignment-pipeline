4bbce59b7ec8b942d13c7e5fa13b8c0b5ffc1de7
Sergei Averkiev <averoo@gmail.com>
2020-08-07 16:35:21 +0300

    Update model dispatcher.

--- client/be/app/models/sententense_transformers_models.py
+++ client/be/app/models/sententense_transformers_models.py
@@ -8,8 +8,10 @@ SENTENCE_TRANSFORMERS_MODEL_PATH = 'sentence_transformers.bin'
 class SentenceTransformersModel():
     def __init__(self):        
         if os.path.isfile(SENTENCE_TRANSFORMERS_MODEL_PATH):
+            print("Loading saved sentence_transformers model.")
             self.model = pickle.load(open(SENTENCE_TRANSFORMERS_MODEL_PATH, 'rb'))
         else:
+            print("Loading sentence_transformers model from Internet.")
             self.model = SentenceTransformer('distiluse-base-multilingual-cased')      
     def embed(self, lines):
         vecs = self.model.encode(lines)
--- client/be/app/models/use_multilingual_models.py
+++ client/be/app/models/use_multilingual_models.py
@@ -6,20 +6,22 @@ import tensorflow_hub as hub
 
 from tensorflow_text import SentencepieceTokenizer
 
-USE_MULTILINGUAL_V3_MODEL_PATH = 'use_multilingual_v3.bin'
+USE_MULTILINGUAL_V3_MODEL_PATH = './models/use_multilingual_models_saved/saved_model.pb'
 
 class UseMultilingualV3():
     def __init__(self):        
         if os.path.isfile(USE_MULTILINGUAL_V3_MODEL_PATH):
-            self.model = pickle.load(open(USE_MULTILINGUAL_V3_MODEL_PATH, 'rb'))
+            print("Loading saved use_multilingual_v3 model.")
+            self.model = tf.saved_model.load("./models/use_multilingual_models_saved")
         else:
-            self.model = hub.load('https://tfhub.dev/google/universal-sentence-encoder-multilingual/3')      
+            print("Loading use_multilingual_v3 model from tf_hub.")
+            self.model = hub.load('https://tfhub.dev/google/universal-sentence-encoder-multilingual/3')
+            
     def embed(self, lines):
         vecs = self.model(lines).numpy()
         return vecs
 
 use_multilingual_v3_model = UseMultilingualV3()
-
 
 # use_multilingual_v3_model = hub.load('https://tfhub.dev/google/universal-sentence-encoder-multilingual/3')
 # pickle.dump(use_multilingual_v3_model, open("use_multilingual_v3.bin", 'wb'))

4cef161b4b72bb2052013b98c0a625d26e79cadf
Sergei Averkiev <averoo@gmail.com>
2020-08-30 13:56:57 +0300

    Update dockerignore.

--- client/fe/app/src/common/config.js
+++ client/fe/app/src/common/config.js
@@ -1,5 +1,5 @@
-export const API_URL = "http://127.0.0.1:80";
-// export const API_URL = "http://164.90.235.113:80";
+//export const API_URL = "http://127.0.0.1:80";
+export const API_URL = "http://95.181.157.66:80";
 export const DEFAULT_BATCHSIZE = "50";
 
 export default API_URL;

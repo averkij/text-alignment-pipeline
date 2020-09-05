3780fb7e96462d69aa64df4320ada4f77cbfb29e
Sergei Averkiev <averoo@gmail.com>
2020-09-05 18:30:01 +0300

    Add translation variants. Styling.

--- client/fe/app/src/components/EditItem.vue
+++ client/fe/app/src/components/EditItem.vue
@@ -3,10 +3,7 @@
     <v-row justify="center" no-gutters>
       <v-col class="text-left" cols="6">
         <div class="d-table fill-height">
-          <div
-            class="d-table-cell grey lighten-4 pa-2 text-center font-weight-medium"
-            style="min-width:45px"
-          >
+          <div class="d-table-cell grey lighten-5 pa-2 text-center font-weight-medium" style="min-width:45px">
             {{ parseInt(item.line_id) + 1 }}
           </div>
           <v-divider class="d-table-cell" vertical></v-divider>
@@ -14,19 +11,14 @@
         </div>
       </v-col>
       <v-col class="text-left" cols="6">
-        <div class="d-table fill-height">
+        <div class="d-table fill-height fill-width">
           <v-divider class="d-table-cell" vertical></v-divider>
-          <div
-            class="d-table-cell grey lighten-5 text-center"
-            style="min-width:45px"
-          >
-            <div
-              class="fill-height lighten-5 d-flex flex-column justify-space-between"
-              :class="{
+          <div class="d-table-cell lighten-5 text-center" style="min-width:45px" :class="{
+                grey: item.selected.sim <= 0.3,
                 green: item.selected.sim > 0.5,
                 yellow: (item.selected.sim <= 0.5) && (item.selected.sim > 0.3)
-              }"
-            >
+              }">
+            <div class="fill-height lighten-5 d-flex flex-column justify-space-between" >
               <div class="pa-2 font-weight-medium">
                 {{ selectedLineId }}
               </div>
@@ -36,10 +28,48 @@
             </div>
           </div>
           <v-divider class="d-table-cell" vertical></v-divider>
-          <div class="d-table-cell pa-2">
-            {{ item.selected.text }}
-            <div v-for="(t,i) in linesTo" :key="i" class="yellow lighten-4">=> {{t.line_id}} [{{t.sim | numeral("0.00")}}] {{ t.text }}</div>
+
+          <div class="d-table-cell" style="width:100%">
+            <v-expansion-panels flat accordion>
+              <v-expansion-panel>
+                <v-expansion-panel-header>{{ item.selected.text }}</v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  <div v-for="(t,i) in linesTo" :key="i">
+                    <v-divider></v-divider>
+                    <div class="d-table">
+                      <div class="d-table-cell lighten-5 pa-2 text-center font-weight-medium" style="min-width:45px"
+                        :class="[{green: t.line_id==item.selected.line_id}, {grey: t.line_id!=item.selected.line_id}]">
+                        {{ t.line_id + 1 }}
+                      </div>
+                      <v-divider class="d-table-cell" vertical></v-divider>
+                      <div class="d-table-cell pa-2">{{ t.text }}</div>
+                    </div>
+                  </div>
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+            </v-expansion-panels>
           </div>
+
+
+          <!-- <div class="d-table-cell">
+            <div class="pa-2">
+              {{ item.selected.text }}
+            </div>
+            <div>
+              <div v-for="(t,i) in linesTo" :key="i">
+                <v-divider></v-divider>
+                <div class="d-table">
+                  <div class="d-table-cell lighten-5 pa-2 text-center font-weight-medium" style="min-width:45px"
+                    :class="[{green: t.line_id==item.line_id}, {grey: t.line_id!=item.line_id}]">
+                    {{ t.line_id + 1 }}
+                  </div>
+                  <v-divider class="d-table-cell" vertical></v-divider>
+                  <div class="d-table-cell pa-2">{{ t.text }}</div>
+                </div>
+              </div>
+            </div>
+          </div> -->
+
         </div>
       </v-col>
     </v-row>
@@ -47,17 +77,17 @@
 </template>
 
 <script>
-import _ from 'lodash'
-export default {
-  name: "EditItem",
-  props: ["item"],
-  computed: {
-    selectedLineId() {
-      return parseInt(this.item.selected.line_id) + 1;
-    },
-    linesTo() {
-      return _.orderBy(this.item.trans, 'line_id')
+  import _ from 'lodash'
+  export default {
+    name: "EditItem",
+    props: ["item"],
+    computed: {
+      selectedLineId() {
+        return parseInt(this.item.selected.line_id) + 1;
+      },
+      linesTo() {
+        return _.orderBy(this.item.trans, 'line_id')
+      }
     }
-  }
-};
+  };
 </script>

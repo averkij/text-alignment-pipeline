e66c026fbd827f47a7875254f7e37a69731f07c8
Sergei Averkiev <averoo@gmail.com>
2020-09-06 11:57:58 +0300

    Add lodash filter for translation variants.

--- client/fe/app/src/components/EditItem.vue
+++ client/fe/app/src/components/EditItem.vue
@@ -18,7 +18,7 @@
                 green: item.selected.sim > 0.5,
                 yellow: (item.selected.sim <= 0.5) && (item.selected.sim > 0.3)
               }">
-            <div class="fill-height lighten-5 d-flex flex-column justify-space-between" >
+            <div class="fill-height lighten-5 d-flex flex-column justify-space-between">
               <div class="pa-2 font-weight-medium">
                 {{ selectedLineId }}
               </div>
@@ -49,27 +49,6 @@
               </v-expansion-panel>
             </v-expansion-panels>
           </div>
-
-
-          <!-- <div class="d-table-cell">
-            <div class="pa-2">
-              {{ item.selected.text }}
-            </div>
-            <div>
-              <div v-for="(t,i) in linesTo" :key="i">
-                <v-divider></v-divider>
-                <div class="d-table">
-                  <div class="d-table-cell lighten-5 pa-2 text-center font-weight-medium" style="min-width:45px"
-                    :class="[{green: t.line_id==item.line_id}, {grey: t.line_id!=item.line_id}]">
-                    {{ t.line_id + 1 }}
-                  </div>
-                  <v-divider class="d-table-cell" vertical></v-divider>
-                  <div class="d-table-cell pa-2">{{ t.text }}</div>
-                </div>
-              </div>
-            </div>
-          </div> -->
-
         </div>
       </v-col>
     </v-row>
@@ -86,7 +65,13 @@
         return parseInt(this.item.selected.line_id) + 1;
       },
       linesTo() {
-        return _.orderBy(this.item.trans, 'line_id')
+        let sid = this.item.selected.line_id;
+        let wnd = 3;
+        return _(this.item.trans)
+          .filter(function (tr) {
+            return tr.line_id < sid + wnd && tr.line_id > sid - wnd
+          })
+          .orderBy('line_id');
       }
     }
   };

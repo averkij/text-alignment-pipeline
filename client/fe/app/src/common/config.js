274cc71f7960a4fb2c2147f6c51481914649dc1a
Sergei Averkiev <averoo@gmail.com>
2020-09-12 14:31:38 +0300

    Config change.

--- client/fe/app/src/common/config.js
+++ client/fe/app/src/common/config.js
@@ -1,6 +1,6 @@
-export const API_URL = "http://127.0.0.1:80";
-// export const API_URL = "http://95.181.157.66:80";
-export const DEFAULT_BATCHSIZE = "50";
+// export const API_URL = "http://127.0.0.1:80";
+export const API_URL = "http://95.181.152.195:80";
+export const DEFAULT_BATCHSIZE = "100";
 export const DEFAULT_VARIANTS_WINDOW_FROM = 5;
 export const DEFAULT_VARIANTS_WINDOW_TO = 20;
 

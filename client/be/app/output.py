f96d06422671cdb3b8c3d39c7236a407f1fd6550
Sergei Averkiev <averoo@gmail.com>
2020-09-13 12:26:54 +0300

    Fix tmx saving.

--- client/be/app/output.py
+++ client/be/app/output.py
@@ -10,11 +10,11 @@ def save_tmx(input_file, output_file, lang_from, lang_to):
     tmx_template = output_templates.TMX_BLOCK.format(timestamp=datetime.utcnow().strftime('%Y%m%dT%H%M%SZ'), \
         culture_from=helper.get_culture(lang_from), culture_to=helper.get_culture(lang_to))
     with open(output_file, mode="w", encoding="utf-8") as doc_out:
-        for line_from, translation, candidates in helper.read_processing(input_file):
-            doc_out.write(output_templates.TMX_BEGIN)
+        doc_out.write(output_templates.TMX_BEGIN)
+        for line_from, translation, candidates in helper.read_processing(input_file):            
             if translation[0].text:
                 doc_out.write(tmx_template.format(text_from=line_from.text.strip(), text_to=translation[0].text.strip()))
-            doc_out.write(output_templates.TMX_END)
+        doc_out.write(output_templates.TMX_END)
 
 def save_plain_text(input_file, output_file, first_lang):
     with open(output_file, mode="w", encoding="utf-8") as doc_out:

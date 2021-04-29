ed785e93add678fd391aef54879f402b3c374e35
Sergei Averkiev <averoO@gmail.com>
2021-04-29 09:07:04 +0300

    Add some texts

--- client/be/app/models/sententense_transformers_models.py
+++ client/be/app/models/sententense_transformers_models.py
@@ -1,55 +1,81 @@
 import os
 import pickle
-from helper import lazy_property
+# from helper import lazy_property
 
 from sentence_transformers import SentenceTransformer
 import torch
 
 # torch.backends.quantized.engine = 'qnnpack'
 
-SENTENCE_TRANSFORMERS_MODEL_PATH = './models/sentence_transformers.bin'
-SENTENCE_TRANSFORMERS_XLM_100_MODEL_PATH = './models/sentence_transformers_xlm_100.bin'
+# SENTENCE_TRANSFORMERS_MODEL_PATH = './models/sentence_transformers.bin'
+# SENTENCE_TRANSFORMERS_XLM_100_MODEL_PATH = './models/sentence_transformers_xlm_100.bin'
 
-class SentenceTransformersModel(): 
-    @lazy_property
-    def model(self):
-        if os.path.isfile(SENTENCE_TRANSFORMERS_MODEL_PATH):
-            print("Loading saved distiluse-base-multilingual-cased model.")
-            # self.model = torch.quantization.quantize_dynamic(pickle.load(open(SENTENCE_TRANSFORMERS_MODEL_PATH, 'rb')), {torch.nn.Linear}, dtype=torch.qint8)
-            _model = pickle.load(open(SENTENCE_TRANSFORMERS_MODEL_PATH, 'rb'))
-        else:
-            print("Loading distiluse-base-multilingual-cased model from Internet.")
-            _model = SentenceTransformer('distiluse-base-multilingual-cased')
-        return _model
+# class SentenceTransformersModel(): 
+#     @lazy_property
+#     def model(self):
+#         if os.path.isfile(SENTENCE_TRANSFORMERS_MODEL_PATH):
+#             print("Loading saved distiluse-base-multilingual-cased model.")
+#             # self.model = torch.quantization.quantize_dynamic(pickle.load(open(SENTENCE_TRANSFORMERS_MODEL_PATH, 'rb')), {torch.nn.Linear}, dtype=torch.qint8)
+#             _model = pickle.load(open(SENTENCE_TRANSFORMERS_MODEL_PATH, 'rb'))
+#         else:
+#             print("Loading distiluse-base-multilingual-cased model from Internet.")
+#             _model = SentenceTransformer('distiluse-base-multilingual-cased')
+#         return _model
              
-    def embed(self, lines):
-        vecs = self.model.encode(lines)
-        return vecs
+#     def embed(self, lines):
+#         vecs = self.model.encode(lines)
+#         return vecs
 
-class SentenceTransformersModelXlm100(): 
-    @lazy_property
-    def model(self):
-        if os.path.isfile(SENTENCE_TRANSFORMERS_XLM_100_MODEL_PATH):
-            print("Loading saved xlm-r-100langs-bert-base-nli-mean-tokens model.")
-            # self.model = torch.quantization.quantize_dynamic(pickle.load(open(SENTENCE_TRANSFORMERS_MODEL_PATH, 'rb')), {torch.nn.Linear}, dtype=torch.qint8)
-            _model = pickle.load(open(SENTENCE_TRANSFORMERS_XLM_100_MODEL_PATH, 'rb'))
-        else:
-            print("Loading xlm-r-100langs-bert-base-nli-mean-tokens model from Internet.")
-            _model = SentenceTransformer('xlm-r-100langs-bert-base-nli-mean-tokens')
-        return _model
+# class SentenceTransformersModelXlm100(): 
+#     @lazy_property
+#     def model(self):
+#         if os.path.isfile(SENTENCE_TRANSFORMERS_XLM_100_MODEL_PATH):
+#             print("Loading saved xlm-r-100langs-bert-base-nli-mean-tokens model.")
+#             # self.model = torch.quantization.quantize_dynamic(pickle.load(open(SENTENCE_TRANSFORMERS_MODEL_PATH, 'rb')), {torch.nn.Linear}, dtype=torch.qint8)
+#             _model = pickle.load(open(SENTENCE_TRANSFORMERS_XLM_100_MODEL_PATH, 'rb'))
+#         else:
+#             print("Loading xlm-r-100langs-bert-base-nli-mean-tokens model from Internet.")
+#             _model = SentenceTransformer('xlm-r-100langs-bert-base-nli-mean-tokens')
+#         return _model
              
-    def embed(self, lines):
-        vecs = self.model.encode(lines)
-        return vecs
+#     def embed(self, lines):
+#         vecs = self.model.encode(lines)
+#         return vecs
 
-sentence_transformers_model = SentenceTransformersModel()
-sentence_transformers_model_xlm_100 = SentenceTransformersModelXlm100()
+# sentence_transformers_model = SentenceTransformersModel()
+# sentence_transformers_model_xlm_100 = SentenceTransformersModelXlm100()
 
 
 
 ###########model saving
 # model = SentenceTransformer('distiluse-base-multilingual-cased')
 # pickle.dump(model, open("sentence_transformers.bin", 'wb'))
+
+###########quantization
+
+import torch.quantization
+import torch
+
+model = SentenceTransformer('distiluse-base-multilingual-cased')
+
+model
+
+# Input to the model
+x = torch.randn(1, 1, 224, 224, requires_grad=True)
+torch_out = model(x)
+
+# Export the model
+torch.onnx.export(model,               # model being run
+                  x,                         # model input (or a tuple for multiple inputs)
+                  "sentence_transformers.onnx",   # where to save the model (can be a file or file-like object)
+                  export_params=True,        # store the trained parameter weights inside the model file
+                  opset_version=10,          # the ONNX version to export the model to
+                  do_constant_folding=True,  # whether to execute constant folding for optimization
+                  input_names = ['input'],   # the model's input names
+                  output_names = ['output'], # the model's output names
+                  dynamic_axes={'input' : {0 : 'batch_size'},    # variable lenght axes
+                                'output' : {0 : 'batch_size'}})
+
 
 ############quantization
 
